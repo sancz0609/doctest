1868cad3249e8a70fb96db1f9129ece1964ea0b7
sanchit <sanchitbathla0609@gmail.com>
2018-03-05 17:41:13 +0530

    updated on natural

--- index.js
+++ index.js
@@ -12,17 +12,28 @@ function getData(){
         btn.classList.add("hide-me");
 
 
-      
-        var baseNouns = obj.baseDocumentStats.nounCount;
-        var baseWordCount = obj.baseDocumentStats.wordCount;
-        var checkNouns = obj.checkDocumentStats.nounCount;
-        var checkWordCount = obj.checkDocumentStats.wordCount;
-        var commonNouns = obj.checkDocumentStats.matchingNounCount;
-        var percentage = Math.round((commonNouns/baseNouns)*100);
 
+        var standardNouns = obj.Standard.nouns;
+        var standardAdjectives = obj.Standard.adjectives;
+        var standardVerbs = obj.Standard.verbs;
+        console.log(obj.Standard.verbs);
+        var standardWordCount = obj.Standard.wordCount;
+        //details of eval document
+        var evalNouns = obj.Eval.nouns;
+        var evalAdjectives = obj.Eval.adjectives;
+        var evalVerbs = obj.Eval.verbs;
+        var evalWordCount = obj.Eval.wordCount;
+        //commmon properties
+        var commonNouns = obj.Common.nouns;
+        var commonAdjectives = obj.Common.adjectives;
+        var commonVerbs = obj.Common.verbs;
+        console.log(commonNouns);
 
+        var total = standardAdjectives+standardNouns+standardVerbs;
+        var marks = commonNouns+commonVerbs+commonAdjectives;
+        var percentage = Math.round((marks/total)*100);
         //html code that needs to be fired on button click
-        var html_code = "<p class = \"new_content\">Comparison complete!!<br> <table id = \"table\"><tr><th>Table</th><th>Word Count</th><th>Nouns<th></tr><tr><th>Your Document</th><td>"+checkWordCount+"</td><td>"+checkNouns+"</td></tr><tr><th>Standard Document</th><td>"+baseWordCount+"</td><td>"+baseNouns+"</td></tr></table><br></p><p class = \"new-content\">Your document has<b> "+commonNouns+"</b> common nouns with our standard document.<br>Your final score : <b>"+percentage+"%</b></p>";
+        var html_code = "<p class = \"new_content\">Comparison complete!!<br> <table id = \"table\"><tr><th>Table</th><th>Word Count</th><th>Nouns</th><th>Adjectives</th><th>Verbs</th></tr><tr><th>Your Document</th><td>"+evalWordCount+"</td><td>"+evalNouns+"</td><td>"+evalAdjectives+"</td><td>"+evalVerbs+"</td></tr><tr><th>Standard Document</th><td>"+standardWordCount+"</td><td>"+standardNouns+"</td><td>"+standardAdjectives+"</td><td>"+standardVerbs+"</td></tr></table><br></p><p class = \"new-content\">Your document has<b> "+commonNouns+"</b> common nouns with our standard document.<br>Your document has<b> "+commonAdjectives+"</b> common adjectives with our standard document.<br>Your document has<b> "+commonVerbs+"</b> common verbs with our standard document.<br><br>Your final score : <b>"+percentage+"%</b></p>";
         //where the content shall be inserted
         document.getElementById('content').insertAdjacentHTML('afterend', html_code);
     }
--- testing.js
+++ testing.js
@@ -7,6 +7,17 @@ var natural = require('natural');
 
 //for tokenizing
 var tokenizer = new natural.WordTokenizer();
+var path = require("path");
+
+var base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger");
+var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt";
+var lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json";
+var defaultCategory = 'N';
+
+var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory);
+var rules = new natural.RuleSet(rulesFilename);
+var tagger = new natural.BrillPOSTagger(lexicon, rules);
+//pos tagger over
 
 var baseDocument;
 var checkDocument;
@@ -14,8 +25,8 @@ var baseLength;
 var checkLength;
 
 //for wordpos
-var WordPOS = require('wordpos'),
-wordpos = new WordPOS();
+//var WordPOS = require('wordpos'),
+//wordpos = new WordPOS();
 
 /*
 // doc extracting is not working
@@ -40,6 +51,16 @@ textract.fromFileWithPath('check.docx', function( error, text ) {
     checkDocument = text;
   }
 });*/
+
+var evalNouns = [];
+var standardNouns = [];
+var evalAdjectives = [];
+var standardAdjectives = [];
+var evalVerbs = [];
+var standardVerbs = [];
+var similarNouns = [];
+var similarAdjectives = [];
+var similarVerbs = [];
 
 //console.log(this.baseDocument);
 //console.log(checkDocument);
@@ -92,6 +113,118 @@ if(checkLength < range_min || checkLength > range_max )
   return;
 }
 
+else{
+  //call for calculating everything
+  calculate(tokenBaseDocument, tokenCheckDocument, end);
+}
+
+//counting nouns, adjectives and vers for both documents to compare
+function calculate(standard, evaluate, callback){
+  var standardTagged = tagger.tag(standard);
+  var evalTagged = tagger.tag(evaluate);
+
+  //for the counting of standard document
+  for(var i=0; i < standardTagged.length; i++){
+    //if its a noun
+    if(standardTagged[i][1] == "NN"|"NNS"|"NNP"|"NNPS")
+      standardNouns.push(standardTagged[i][0]);
+    //if its an adjective
+    else if(standardTagged[i][1] == "JJ"|"JJR"|"JJS")
+      standardAdjectives.push(standardTagged[i][0]);
+    //if its a verb
+    else if(standardTagged[i][1] == "VB"|"VBD"|"VBG"|"VBN"|"VBP")
+      standardVerbs.push(standardTagged[i][0]);
+  }
+    //for the counting of evaluation document
+  for(var i=0; i < evalTagged.length; i++){
+    //if its a noun
+    if(evalTagged[i][1] == "NN"|"NNS"|"NNP"|"NNPS")
+      evalNouns.push(evalTagged[i][0]);
+    //if its an adjective
+    else if(evalTagged[i][1] == "JJ"|"JJR"|"JJS")
+      evalAdjectives.push(evalTagged[i][0]);
+    //if its a verb
+    else if(evalTagged[i][1] == "VB"|"VBD"|"VBG"|"VBN"|"VBP")
+      evalVerbs.push(evalTagged[i][0]);
+
+  }
+
+   compareNouns();
+   compareAdjectives();
+   compareVerbs();
+   end();
+  }
+
+//comparing nouns of both documents and calculating noun percentage
+function compareNouns(){
+  var corpus = standardNouns;
+  var spellcheck = new natural.Spellcheck(corpus);
+  //comparing each noun of evalDoc with corpus
+  for(let i = 0; i < evalNouns.length; i++){
+	if(spellcheck.isCorrect(evalNouns[i])){
+		similarNouns.push(evalNouns[i]);
+	}
+}
+
+}
+
+function compareAdjectives(){
+  var corpus = standardAdjectives;
+  var spellcheck = new natural.Spellcheck(corpus);
+  //comparing each noun of evalDoc with corpus
+  for(let i = 0; i < evalAdjectives.length; i++){
+	if(spellcheck.isCorrect(evalAdjectives[i])){
+		similarAdjectives.push(evalAdjectives[i]);
+	}
+}
+}
+
+function compareVerbs(){
+  var corpus = standardVerbs;
+  var spellcheck = new natural.Spellcheck(corpus);
+  //comparing each noun of evalDoc with corpus
+  for(let i = 0; i < evalVerbs.length; i++){
+	if(spellcheck.isCorrect(evalVerbs[i])){
+		similarVerbs.push(evalVerbs[i]);
+	}
+}
+}
+
+
+
+//json file -- to be executed in the end
+ function end(){
+    var score = {
+      Standard :
+        {
+          wordCount : baseLength,
+          nouns : standardNouns.length,
+          adjectives : standardAdjectives.length,
+          verbs : standardVerbs.length
+        },
+
+      Eval :
+        {
+          wordCount : checkLength,
+          nouns : evalNouns.length,
+          adjectives : evalAdjectives.length,
+          verbs : evalVerbs.length
+        },
+      Common :
+      {
+          nouns : similarNouns.length,
+          adjectives : similarAdjectives.length,
+          verbs : similarVerbs.length
+      }
+
+    }
+      //object to string
+  var json = JSON.stringify(score, null, 2);
+  //storing it in .json file
+  fs.writeFileSync('output.json',json)
+ }
+
+/*
 var baseNouns;
 var checkNouns;
 
